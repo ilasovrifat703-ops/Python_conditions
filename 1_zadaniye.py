9d8ccc1c26c537c492e4250b106b59271b71dd82
ilasovrifat703-ops <ilasovrifat703@gmail.com>
2026-09-12 11:25:08 +0300

    Update 1_zadaniye.py

--- 1_zadaniye.py
+++ 1_zadaniye.py
@@ -1,5 +1,6 @@
 import math 
 
+
 a = int(input('Введите значение a: '))
 b = int(input('Введите значение b: '))
 x = int(input('Введите значение x: '))
@@ -8,4 +9,5 @@ if 3*b**2 > a:
     y = math.e**math.sin(x) + b
 else:
     y = -(math.e)**(-x) + a*math.log10(x)
+
 print(y)
