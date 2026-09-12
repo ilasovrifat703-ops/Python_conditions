3b95c220708b8e5ddb80dbc0e1a1f2c869ac5f29
ilasovrifat703-ops <ilasovrifat703@gmail.com>
2026-09-12 11:39:11 +0300

    Create 2_task.py

---
+++ 2_task.py
@@ -0,0 +1,32 @@
+num_year = int(input('ВВедите номер года: '))
+
+diff_years = abs(num_year-1996)%12
+
+match diff_years:
+    case 0:
+        print('Год крысы')
+    case 1:
+        print('Год коровы')
+    case 2:
+        print('Год тигра')
+    case 3:
+        print('Год зайца')
+    case 4:
+        print('Год дракона')
+    case 5:
+        print('Год змеи')
+    case 6:
+        print('Год лошади')
+    case 7:
+        print('Год овцы')
+    case 8:
+        print('Год обезбяны')
+    case 9:
+        print('Год петуха')
+    case 10:
+        print('Год собаки')
+    case 11:
+        print('Год свиньи')
+
+
+    
