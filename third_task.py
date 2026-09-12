007ccb08f886dc3481220d6d542123ac7eef6241
ilasovrifat703-ops <ilasovrifat703@gmail.com>
2026-09-12 16:52:24 +0300

    fix

---
+++ third_task.py
@@ -0,0 +1,6 @@
+x,y = map(float,input('Введите значения x и y через пробел: ').split())
+
+def func(x,y):
+    return True if (x**2 + y**2 <= 2**2 and x <= 0) or (x**2 + y**2 > 1 and x**2 + y**2 <= 2**2 and x >= 0) else False
+
+print(func(x,y))
